8ef21c86df56386e0df2a7a46786c15c87d6853e
Kauã Victor <kauavictor259@gmail.com>
2021-11-23 00:13:56 -0300

    configurado o htaccess

--- index.php
+++ index.php
@@ -7,4 +7,8 @@ require PASTA_RAIZ . '/vendor/autoload.php';
 use \App\Controller\View\Home;
 use \App\Controller\View\Page;
 
+echo '<pre>';
+print_r($_GET);
+echo '</pre>';
+
 echo Page::getPage();
